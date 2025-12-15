c204737d2e9f721dee82735ed2d9dc315c445f4d
karemahamdy20 <K.Hamdy@tera.ltd>
2025-12-15 14:22:17 +0200

    add validation for user group form

--- src/modules/user-group/pages/UserGroupForm.vue
+++ src/modules/user-group/pages/UserGroupForm.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import ScreenHeader from "@/sharedComponents/ScreenHeader.vue";
 import BaseButton from "@/sharedComponents/BaseButton.vue";
 import { useRoute } from "vue-router";
+
+import { useForm } from "vee-validate";
+import { groupFormSchema } from "../validation/GroupsListSchema";
 
 const props = defineProps<{
   mode: "edit" | "create";
@@ -10,46 +12,80 @@ const props = defineProps<{
 
 const editMode = props.mode === "edit";
 const route = useRoute();
-const groupId = ref<string | null>(route.params.id ? String(route.params.id) : null);
-const groupName = ref<string>("");
-const description = ref<string>("");
 
-const handleSubmit = () => {
-  console.log(`form submited ${groupId.value}`);
-}
+// const groupId = route.params.id
+//   ? String(route.params.id)
+//   : null;
+
+const { handleSubmit, errors, defineField } = useForm({
+  validationSchema: groupFormSchema,
+  initialValues: {
+    groupName: "",
+    description: "",
+  },
+});
+
+const [groupName] = defineField("groupName");
+const [description] = defineField("description");
+const onSubmit = handleSubmit((values) => {
+  console.log("Form Values", values);
+});
 </script>
+
 <template>
   <div>
     <ScreenHeader title="accessControl" subtitle="userGroup.userGroup" />
-    <card class="p-6 bg-[#ffffff] rounded-[10px] ">
+
+    <card class="p-6 bg-[#ffffff] rounded-[10px]">
       <template #title>
-        <div class="flex flex-col  px-20">
-          <h2 class="heading-title">{{ editMode ? $t("userGroup.editUserGroup") : $t("userGroup.addNewUserGroup") }}
+        <div class="flex flex-col px-20">
+          <h2 class="heading-title">
+            {{ editMode ? $t("userGroup.editUserGroup") : $t("userGroup.addNewUserGroup") }}
           </h2>
           <p class="subheading-title">
             {{ $t("userGroup.userGroupInfo") }}
           </p>
         </div>
       </template>
+
       <template #content>
-        <form @submit.prevent="handleSubmit" class="space-y-6 px-20">
+        <form @submit.prevent="onSubmit" class="space-y-6 px-20">
           <div>
-            <label class="text-gray-700 font-medium mb-2 block">{{ $t("userGroup.groupName") }}</label>
-            <InputText v-model="groupName" placeholder="e.g., Finance Team"
-              class="mt-1 w-full p-3 border border-gray-300 rounded-lg" />
+            <label class="text-gray-700 font-medium mb-2 block">
+              {{ $t("userGroup.groupName") }}
+            </label>
+
+            <InputText v-model="groupName" placeholder="e.g., Finance Team" class="mt-1 w-full p-3 border rounded-lg"
+              :class="{ 'border-danger-500': errors.groupName }" />
+
+            <small v-if="errors.groupName" class="text-danger-500">
+              {{ errors.groupName }}
+            </small>
           </div>
           <div>
-            <label class="text-gray-700 font-medium mb-2 block">{{ $t("userGroup.description") }}</label>
+            <label class="text-gray-700 font-medium mb-2 block">
+              {{ $t("userGroup.description") }}
+            </label>
+
             <Textarea v-model="description" :placeholder="$t('userGroup.descriptionPlaceholder')"
-              class="mt-1 w-full p-3 border border-gray-300 rounded-lg" rows="4" />
+              class="mt-1 w-full p-3 border rounded-lg" rows="4" :class="{ 'border-danger-500': errors.description }" />
+
+            <small v-if="errors.description" class="text-danger-500">
+              {{ errors.description }}
+            </small>
           </div>
+
           <div class="flex justify-between gap-4 mb-4 w-full">
             <BaseButton label="button.cancel" variant="ghost" block :to="{ name: 'UserGroup' }" />
-            <BaseButton :label="editMode ? 'button.save' : 'userGroup.createGroup'" variant="primary" block />
+
+            <BaseButton type="submit" :label="editMode ? 'button.save' : 'userGroup.createGroup'" variant="primary"
+              block />
           </div>
+
         </form>
       </template>
     </card>
   </div>
 </template>
+
 <style scoped></style>
