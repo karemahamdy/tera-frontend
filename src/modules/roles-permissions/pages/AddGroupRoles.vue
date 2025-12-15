a6fb7990bd533ca4199d25f7f16dd0eafa231e32
karemahamdy20 <K.Hamdy@tera.ltd>
2025-12-15 14:37:36 +0200

    add user roles schema

--- src/modules/roles-permissions/pages/AddGroupRoles.vue
+++ src/modules/roles-permissions/pages/AddGroupRoles.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
 import ScreenHeader from "@/sharedComponents/ScreenHeader.vue";
 import BaseButton from "@/sharedComponents/BaseButton.vue";
-import { ref } from "vue";
 
-const accessScope = ref<string>("branch");
-const name = ref<string>("");
-const options = ref<Array<{ label: string; value: string }>>([
+import { useForm } from "vee-validate";
+import { assignRolesSchema } from "../validation/AssignRolesSchema";
+
+/* dropdown options */
+const options = [
   { label: "Admin", value: "admin" },
   { label: "Editor", value: "editor" },
   { label: "Viewer", value: "viewer" },
-]);
-const selectedOption = ref<{ label: string; value: string } | null>(null);
+];
+
+/* vee-validate */
+const { handleSubmit, errors, defineField } = useForm({
+  validationSchema: assignRolesSchema,
+  initialValues: {
+    name: "",
+    role: null,
+    roles: null,
+    accessScope: "branch",
+  },
+});
+
+/* fields */
+const [name] = defineField("name");
+const [role] = defineField("role");
+const [roles] = defineField("roles");
+const [accessScope] = defineField("accessScope");
+
+/* submit */
+const onSubmit = handleSubmit((values) => {
+  console.log("Assign Roles Payload 👉", values);
+});
 </script>
+
 
 <template>
   <div class="p-6 w-full h-full bg-gray-100">
@@ -25,65 +48,73 @@ const selectedOption = ref<{ label: string; value: string } | null>(null);
       </template>
       <template #content>
         <div class="px-20 mb-6">
-          <form class="space-y-5 ">
-
+          <form class="space-y-5" @submit.prevent="onSubmit">
             <div>
-              <label class="text-gray-700 font-bold">{{
-                $t("userGroup.userGroup")
-                }}</label>
-              <InputText v-model="name" autocomplete="username" placeholder="Finance Team"
-                class="mt-1 w-full p-3 border border-gray-300 rounded-lg"
-                style=" background-color: var(--color-gray-100);" />
+              <label class="text-gray-700 font-bold">
+                {{ $t("userGroup.userGroup") }}
+              </label>
+
+              <InputText v-model="name" placeholder="Finance Team" class="mt-1 w-full p-3 border rounded-lg"
+                :class="{ 'border-danger-500': errors.name }" />
+
+              <small v-if="errors.name" class="text-danger-500">
+                {{ errors.name }}
+              </small>
             </div>
             <div>
-              <label class="text-gray-700 font-bold">{{
-                $t("roles.roleName")
-                }}</label>
+              <label class="text-gray-700 font-bold">
+                {{ $t("roles.roleName") }}
+              </label>
 
-              <Dropdown v-model="selectedOption" :options="options" optionLabel="label"
-                :placeholder="$t('select roles')" class="w-full mt-1" />
+              <Dropdown v-model="role" :options="options" optionLabel="label" class="w-full mt-1"
+                :class="{ 'p-invalid': errors.role }" :placeholder="$t('select roles')" />
+
+              <small v-if="errors.role" class="text-danger-500">
+                {{ errors.role }}
+              </small>
             </div>
-
             <div class="flex flex-col gap-4 w-full">
+              <label class="text-gray-700 font-bold">
+                {{ $t("roles.accessScope") }}
+              </label>
 
-              <!-- Global Access -->
-              <label class="text-gray-700 font-bold">{{
-                $t("roles.accessScope")
-                }}</label>
-              <div
-                class="flex items-center justify-between border rounded-xl px-4 py-4 cursor-pointer hover:border-primary-300 transition"
-                :class="accessScope === 'global' ? 'border-primary-400 bg-primary-25' : 'border-gray-300'"
-                @click="accessScope = 'global'">
+              <div class="flex items-center justify-between border rounded-xl px-4 py-4 cursor-pointer" :class="accessScope === 'global'
+                ? 'border-primary-400 bg-primary-25'
+                : 'border-gray-300'" @click="accessScope = 'global'">
                 <div class="flex items-center gap-3">
                   <RadioButton inputId="global" name="access" value="global" v-model="accessScope" />
-                  <div class="flex flex-col">
-                    <label class="font-medium text-gray-800 cursor-pointer">{{ $t("roles.globalAccess") }}</label>
-                  </div>
+                  <label class="font-medium cursor-pointer">
+                    {{ $t("roles.globalAccess") }}
+                  </label>
                 </div>
               </div>
 
-              <!-- Branch-Specific -->
-              <div
-                class="flex items-center justify-between border rounded-xl px-4 py-4 cursor-pointer hover:border-primary-300 transition"
-                :class="accessScope === 'branch' ? 'border-primary-400 bg-primary-25' : 'border-gray-300'"
-                @click="accessScope = 'branch'">
+              <div class="flex items-center justify-between border rounded-xl px-4 py-4 cursor-pointer" :class="accessScope === 'branch'
+                ? 'border-primary-400 bg-primary-25'
+                : 'border-gray-300'" @click="accessScope = 'branch'">
                 <div class="flex items-center gap-3">
                   <RadioButton inputId="branch" name="access" value="branch" v-model="accessScope" />
-                  <div class="flex flex-col">
-                    <label class="font-medium text-gray-800 cursor-pointer">{{ $t("roles.branchSpecific") }}</label>
-                  </div>
+                  <label class="font-medium cursor-pointer">
+                    {{ $t("roles.branchSpecific") }}
+                  </label>
                 </div>
               </div>
 
+              <small v-if="errors.accessScope" class="text-danger-500">
+                {{ errors.accessScope }}
+              </small>
             </div>
-
             <div>
-              <label class="text-gray-700 font-bold">{{
-                $t("roles.roles")
-                }}</label>
+              <label class="text-gray-700 font-bold">
+                {{ $t("roles.roles") }}
+              </label>
 
-              <Dropdown v-model="selectedOption" :options="options" optionLabel="label"
-                :placeholder="$t('select roles')" class="w-full mt-1" />
+              <Dropdown v-model="roles" :options="options" optionLabel="label" class="w-full mt-1"
+                :class="{ 'p-invalid': errors.roles }" :placeholder="$t('select roles')" />
+
+              <small v-if="errors.roles" class="text-danger-500">
+                {{ errors.roles }}
+              </small>
             </div>
           </form>
         </div>
@@ -91,7 +122,7 @@ const selectedOption = ref<{ label: string; value: string } | null>(null);
       <template #footer>
         <div class="flex justify-between gap-4 mb-4 container px-20">
           <BaseButton label="cancel" variant="ghost" block :to="{ name: 'UserGroup' }" />
-          <BaseButton label="Assign" variant="primary" block />
+          <BaseButton label="Assign" variant="primary" block @click="onSubmit" />
         </div>
       </template>
     </card>
