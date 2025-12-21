00355e64e0766737df670594850067501c5c51f2
beshoyfayek <Beshoy.Medhat@tera.ltd>
2025-12-21 10:39:42 +0200

    fix group service api ts errors

--- src/modules/user-group/services/groupService.ts
+++ src/modules/user-group/services/groupService.ts
@@ -1,26 +1,26 @@
 import axiosWrapper from "@/app/http/axiosWrapper";
-import type { AddGroup, GroupApiItem } from "../types/groups";
+import type { AddGroup, GroupApiItem, ListResponse, ItemResponse } from "../types/groups";
 
 export const GroupService = {
   async getAll(pageIndex = 1): Promise<GroupApiItem[]> {
-    const data = await axiosWrapper.get(
+    const data = await axiosWrapper.get<ListResponse>(
       `/Group/GetAllGroup?PagenationDto.PageIndex=${pageIndex}`
     );
     return data.data.items;
   },
 
   async getById(id: string): Promise<GroupApiItem> {
-    const data = await axiosWrapper.get(`/Group/GetGroupById/${id}`);
+    const data = await axiosWrapper.get<ItemResponse>(`/Group/GetGroupById/${id}`);
     return data.data;
   },
 
   async create(payload: AddGroup) {
-    const data = await axiosWrapper.post(`/Group/CreateGroup`, payload);
+    const data = await axiosWrapper.post<any>(`/Group/CreateGroup`, payload);
     return data.data;
   },
 
  async toggleActive(groupId: string, isActive: boolean) {
-    const data = await axiosWrapper.post(`/Group/GroupActivation`, {
+    const data = await axiosWrapper.post<any>(`/Group/GroupActivation`, {
       groupId,
       isActive
     });
@@ -28,7 +28,7 @@ export const GroupService = {
   },
 
   async update(id: string, payload: AddGroup) {
-    const data = await axiosWrapper.put(`/Group/UpdateGroup/${id}`, payload);
+    const data = await axiosWrapper.put<any>(`/Group/UpdateGroup/${id}`, payload);
     return data.data;
   },
 
