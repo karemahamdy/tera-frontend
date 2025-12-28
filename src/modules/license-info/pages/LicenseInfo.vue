06525ec80c0a08eb92b5a70c48a8905c00aa9ad0
karemahamdy20 <k.hamdy@tera.ltd>
2025-12-28 09:58:53 +0200

    .

--- src/modules/license-info/pages/LicenseInfo.vue
+++ src/modules/license-info/pages/LicenseInfo.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import StatusCard from './components/StatusCard.vue'
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+ 
 
 const cards = ref([
     {
         variant: 'orange',
         icon: 'Clock',
-        title: 'License Expire',
+        title: t('licenseInfo.licenseExpire'),
         value: '65',
         valueUnit: '/ Days',
-        subtitle: 'Expires Dec 31, 2025',
-        statusText: 'Urgent',
+        subtitle: t('licenseInfo.expiresDec312025'),
+        statusText: t('licenseInfo.urgent'),
         progressPercent: 65
     },
     {
         variant: 'green',
         borderClass: 'border-emerald-300',
         icon: 'Profile2User',
-        title: 'User Capacity',
+        title: t('licenseInfo.userCapacity'),
         value: '96/100',
-        subtitle: 'Approaching Limit',
+        subtitle: t('licenseInfo.approachingLimit'),
         statusText: 'Active',
         progressPercent: 96
     },
@@ -27,9 +31,9 @@ const cards = ref([
         variant: 'blue',
         borderClass: 'border-blue-300',
         icon: 'VideoTick',
-        title: 'Concurrent Session',
+        title: t('licenseInfo.concurrentSession'),
         value: '24/40',
-        subtitle: 'Steady High Usage',
+        subtitle: t('licenseInfo.steadyHighUsage'),
         statusText: 'Active',
         progressPercent: 60
     }
