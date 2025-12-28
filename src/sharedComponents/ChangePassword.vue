9de62fb12cadfb68ff122f9827dd058c94608576
karemahamdy20 <k.hamdy@tera.ltd>
2025-12-28 10:03:01 +0200

    .

--- src/sharedComponents/ChangePassword.vue
+++ src/sharedComponents/ChangePassword.vue
@@ -24,6 +24,14 @@ const confirmAction = () => {
   :subtitle="$t('changePassword.subtitle')" @cancel="closeDialog" @confirm="confirmAction">
 
     <form class="space-y-5">
+      <!-- <div>
+        <label class="text-gray-700 font-medium">{{
+          $t("auth.password")
+        }}</label>
+        <Password v-model="password" toggleMask :inputProps="{ autocomplete: 'current-password' }"
+          :placeholder="$t('auth.passwordPlaceholder')" class="mt-1 w-full"
+          inputClass="w-full p-3 border border-gray-300 rounded-lg" />
+      </div> -->
       <div>
         <label class="text-gray-700 font-medium">{{
           $t("auth.password")
@@ -34,18 +42,10 @@ const confirmAction = () => {
       </div>
       <div>
         <label class="text-gray-700 font-medium">{{
-          $t("auth.password")
+          $t("auth.confirmPassword")
         }}</label>
         <Password v-model="password" toggleMask :inputProps="{ autocomplete: 'current-password' }"
-          :placeholder="$t('auth.passwordPlaceholder')" class="mt-1 w-full"
-          inputClass="w-full p-3 border border-gray-300 rounded-lg" />
-      </div>
-      <div>
-        <label class="text-gray-700 font-medium">{{
-          $t("auth.password")
-        }}</label>
-        <Password v-model="password" toggleMask :inputProps="{ autocomplete: 'current-password' }"
-          :placeholder="$t('auth.passwordPlaceholder')" class="mt-1 w-full"
+          :placeholder="$t('auth.newpasswordPlaceholder')" class="mt-1 w-full"
           inputClass="w-full p-3 border border-gray-300 rounded-lg" />
       </div>
     </form>
