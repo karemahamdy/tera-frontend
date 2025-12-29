f44aad2c1a0141ca6f9703c0c10570ee55e4e87b
beshoyfayek <Beshoy.Medhat@tera.ltd>
2025-12-29 12:01:39 +0200

    add user list

---
+++ src/modules/user-management/composables/useUsers.ts
@@ -0,0 +1,90 @@
+import { ref } from "vue";
+import router from "@/app/router";
+
+import type { Pagination, UserListItem } from "../types/User";
+import { UserService } from "../services/user.service";
+import { toastService } from "@/app/services/toastService";
+import { useI18n } from "vue-i18n";
+
+export function useUsers() {
+  const { t } = useI18n();
+
+  const list = ref<UserListItem[]>([]);
+
+  const loading = ref(false);
+
+  const pagination = ref<Pagination>({
+    GroupFilter: undefined,
+    StatusFilter: undefined,
+    ScopeFilter: undefined,
+    DepartmantFilter: undefined,
+    PageIndex: 1,
+    PageSize: 10,
+    SearchingWord: undefined,
+    OrderBy: undefined,
+    OrderDirection: undefined,
+    total: 0,
+  });
+
+  const getList = async () => {
+    loading.value = true;
+    try {
+      const res = await UserService.getList(pagination.value);
+      list.value = res.data.items;
+      console.log(res.data.items);
+      
+      pagination.value.total = res.data.totalCount;
+    } catch (error) {
+      toastService.error(error as string);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const changePage = async (page: number) => {
+    pagination.value.PageIndex = page;
+    await getList();
+  };
+
+  const search = async (keyword: string) => {
+    pagination.value.SearchingWord = keyword;
+    pagination.value.PageIndex = 1;
+    await getList();
+  };
+
+  const sort = async (orderData: {
+    orderBy: string;
+    direction: "asc" | "desc";
+  }) => {
+    pagination.value.OrderBy = orderData.orderBy;
+    pagination.value.OrderDirection = orderData.direction;
+    pagination.value.PageIndex = 1;
+    await getList();
+  };
+
+  const deleteItem = async (id: string) => {
+    loading.value = true;
+    try {
+      await UserService.delete(id);
+      list.value = list.value.filter((item) => item.userId !== id);
+      pagination.value.total--;
+      toastService.success(t("users.userDeleted"));
+      await getList();
+    } catch (error) {
+      toastService.error(error as string);
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return {
+    list,
+    loading,
+    pagination,
+    getList,
+    changePage,
+    search,
+    sort,
+    deleteItem,
+  };
+}
