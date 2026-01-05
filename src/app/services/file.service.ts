376f9c0666e3cf462b6c5579d5ffa8053cf5ca4a
beshoyfayek <Beshoy.Medhat@tera.ltd>
2026-01-05 16:50:49 +0200

    catch download error

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,20 +1,26 @@
 import axiosWrapper from "@/app/http/axiosWrapper";
+import { toastService } from "./toastService";
 
 export const FileService = {
-  async downloadFile(apiURL: string, fileName?: string) {
-    const response = await axiosWrapper.get<Blob>(apiURL, {
-      responseType: "blob",
-    });
-    const blob = new Blob([response]);
-    const url = window.URL.createObjectURL(blob);
+  downloadFile(url: string, fileName?: string) {
+    return axiosWrapper
+      .get<Blob>(url, {
+        responseType: "blob",
+      })
+      .then((response) => {
+        const blob = new Blob([response]);
+        const url = window.URL.createObjectURL(blob);
 
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = fileName || "user-template-file.csv";
-    document.body.appendChild(link);
-    link.click();
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = fileName || "user-template-file.csv";
+        document.body.appendChild(link);
+        link.click();
 
-    document.body.removeChild(link);
-    window.URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+      }).catch((error) => {
+        toastService.error(error as string);
+      });
   },
 };
