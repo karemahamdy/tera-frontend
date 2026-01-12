a03f63541cc05a119a67efe1f4a2b95bc560abfe
beshoyfayek <Beshoy.Medhat@tera.ltd>
2026-01-12 12:54:31 +0200

    new v

--- src/modules/roles-permissions/services/user.service.ts
+++ src/modules/roles-permissions/services/user.service.ts
@@ -27,24 +27,4 @@ export const UserRolesService = {
     }
     return axiosWrapper.put<AssignRole>("/Users/UpdateAssignRolesToUser", data);
   },
-
-  // getList(params: Pagination) {
-  //   return axiosWrapper.get<RoleListResponse>("/Role/GetAllRole", { params });
-  // },
-
-  // getById(id: string) {
-  //   return axiosWrapper.get<{ data: RoleByID }>(`/Role/${id}`);
-  // },
-
-  // update(id: string, payload: RoleByID) {
-  //   let data = {
-  //     updateRoleDto: payload,
-  //     id: id,
-  //   };
-  //   return axiosWrapper.put<RoleByID>(`/Role/${id}`, data);
-  // },
-
-  // delete(id: string) {
-  //   return axiosWrapper.delete(`/Role/${id}`);
-  // },
 };
