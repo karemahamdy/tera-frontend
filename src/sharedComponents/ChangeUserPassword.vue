40a150d2be225f812e82a4507aff0273cefd7f00
beshoyfayek <Beshoy.Medhat@tera.ltd>
2026-01-17 15:46:22 +0200

    change password

---
+++ src/sharedComponents/ChangeUserPassword.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref } from "vue";
+import BaseDialog from "./BaseDialog.vue";
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+import FormInput from "@/sharedComponents/inputs/FormInput.vue";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+const visible = ref(false);
+
+const emit = defineEmits(["update:visible", "passwordChanged"]);
+
+const closeDialog = () => {
+  emit("update:visible", false);
+};
+
+const userSchema = yup.object({
+  oldPassword: yup.string().required(t("form.fieldRequired")).min(6),
+  newPassword: yup.string().required(t("form.fieldRequired")).min(6),
+  confirmPassword: yup
+    .string()
+    .required(t("form.fieldRequired"))
+    .oneOf([yup.ref("newPassword")], t("form.passwordsMustMatch")),
+});
+
+const { handleSubmit, errors, defineField } = useForm({
+  validationSchema: userSchema,
+});
+
+const [oldPassword] = defineField("oldPassword");
+const [newPassword] = defineField("newPassword");
+const [confirmPassword] = defineField("confirmPassword");
+
+const onSubmit = handleSubmit((values) => {
+  emit("passwordChanged", values);
+  closeDialog();
+});
+</script>
+
+<template>
+  <BaseDialog
+    v-model:visible="visible"
+    @update:visible="closeDialog"
+    :title="$t('changePassword.title')"
+    :subtitle="$t('auth.resetPasswordInfo')"
+    @cancel="closeDialog"
+    @confirm="onSubmit"
+  >
+    <form @submit.prevent="onSubmit" class="space-y-5">
+      <div>
+        <FormInput
+          class="mt-1 w-full"
+          type="password"
+          :label="$t('auth.oldPassword')"
+          v-model="oldPassword"
+          :error="errors.oldPassword"
+          placeholder="****"
+        />
+
+        <FormInput
+          class="mt-1 w-full"
+          type="password"
+          :label="$t('auth.newpassword')"
+          v-model="newPassword"
+          :error="errors.newPassword"
+          placeholder="****"
+        />
+
+        <FormInput
+          class="mt-1 w-full"
+          type="password"
+          :label="$t('auth.confirmPassword')"
+          v-model="confirmPassword"
+          :error="errors.confirmPassword"
+          placeholder="****"
+        />
+      </div>
+    </form>
+  </BaseDialog>
+</template>
