dc2df4e7f448e46992866f45a38873d4aa9fa74c
karemahamdy20 <K.Hamdy@tera.ltd>
2026-01-24 14:52:58 +0200

    fix ts

--- src/modules/Inventory/MasterData/warehouses/components/WarehouseZones.vue
+++ src/modules/Inventory/MasterData/warehouses/components/WarehouseZones.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import { useForm } from "vee-validate";
 import type { Zone, Location } from "../types/warehouse";
 
