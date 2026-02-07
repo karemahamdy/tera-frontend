a9f194ae30cf04782711d2ceba54662359683215
karemahamdy20 <K.Hamdy@tera.ltd>
2026-02-07 12:26:10 +0200

    list

---
+++ src/modules/Inventory/Transactions/purchase-waybill/types/PurchaseWaybill.ts
@@ -0,0 +1,32 @@
+export interface PurchaseWaybill {
+  id: string;
+  code: string;
+  name: string;
+  isActive: boolean;
+  createAt: string;
+}
+
+export interface PurchaseWaybillResponse {
+  data: {
+    items: PurchaseWaybill[];
+    PageIndex: number;
+    pageSize: number;
+    totalCount: number;
+    totalPages: number;
+  };
+  succeeded: boolean;
+  message: string | null;
+  statusCode: number;
+  errorCode: string | null;
+  errors: any;
+  id: string | null;
+}
+
+export interface Pagination {
+  pageIndex: number;
+  pageSize?: number;
+  searchingWord?: string;
+  orderBy?: string;
+  orderDirection?: "asc" | "desc";
+  StatusFilter?: string;
+}
