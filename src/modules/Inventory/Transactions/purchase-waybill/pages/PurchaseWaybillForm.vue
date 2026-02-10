1e53ec6ed190a00687ebe08ca34b07c1bbc4f820
karemahamdy20 <K.Hamdy@tera.ltd>
2026-02-10 15:47:39 +0200

    two tabs

---
+++ src/modules/Inventory/Transactions/purchase-waybill/pages/PurchaseWaybillForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import SupplierDetails from '../components/SupplierDetails.vue';
+import WarehouseDetails from '../components/WarehouseDetails.vue';
+
+
+</script>
+
+<template>
+    <div class="p-6 w-full h-full bg-gray-100">
+              <ScreenHeader title="inventory" subtitle="masterData" actionName="purchaseWaybill.purchaseWaybill" />
+        <card class="bg-[#ffffff] rounded-[10px]">
+            <template #title>
+           
+            </template>
+            <template #content>
+             <SupplierDetails/>
+            </template>
+            <template #footer>
+            <WarehouseDetails/>
+           </template>
+        </card>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
