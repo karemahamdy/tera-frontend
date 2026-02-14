bc11995e8f0840f4de0dd040f6980dde6a5de722
karemahamdy20 <K.Hamdy@tera.ltd>
2026-02-14 12:13:49 +0200

    purshase return expcept summary

--- src/modules/Inventory/Transactions/purchase-return/pages/PurchaseReturnForm.vue
+++ src/modules/Inventory/Transactions/purchase-return/pages/PurchaseReturnForm.vue
@@ -1,22 +1,48 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import ReturnDetails from '../components/ReturnDetails.vue';
 
+import BaseStepper from '@/sharedComponents/stepper/BaseStepper.vue';
+import StepperActions from '@/sharedComponents/stepper/StepperActions.vue';
+import LineItems from '../components/LineItems.vue';
+import QualityInspection from '../components/QualityInspection.vue';
+
+const activeStep = ref(0);
+const nextTab = () => {
+  if (activeStep.value < steps.length - 1) activeStep.value++;
+};
+
+const previousTab = () => {
+  if (activeStep.value > 0) activeStep.value--;
+};
+
+const submit = () => {
+  console.log("finish wizard");
+};
+
+const steps = [
+  { label: "Return Information" },
+  { label: "Return Items" },
+  { label: "Review" }
+];
 </script>
 
 <template>
-    <div class="p-6 w-full h-full bg-gray-100">
-        <ScreenHeader title="inventory" subtitle="masterData" actionName="purchaseReturn.purchaseReturn" />
-        <card class="bg-[#ffffff] rounded-[10px]">
-            <template #title>
-               <h1>gygyyggy</h1>
-            </template>
-            <template #content>
-               <h1>gygyyggy</h1> 
-            </template>
-           
-        </card>
-    </div>
+  <div class="p-6 w-full h-full bg-gray-100">
+    <ScreenHeader title="inventory" subtitle="masterData" actionName="purchaseWaybill.purchaseWaybill" />
+    <BaseStepper v-model="activeStep" :steps="steps" code="PW-2026-001">
+      <Card class="mt-6 rounded-2xl shadow-sm">
+        <template #content>
+          <ReturnDetails v-if="activeStep === 0" />
+          <LineItems v-else-if="activeStep === 1" @next="nextTab" @prev="previousTab" />
+          <QualityInspection v-else-if="activeStep === 2" @prev="previousTab" @submit="submit" />
+        </template>
+      </Card>
+      <StepperActions :current="activeStep" :total="steps.length" nextText="Next" prevText="Back" finishText="Create"
+        @next="nextTab" @previous="previousTab" @finish="submit" />
+
+    </BaseStepper>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
