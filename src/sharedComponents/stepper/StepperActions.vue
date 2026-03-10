94399c9950ba67ad61db2a8081b3a1d0513998cb
karemahamdy20 <K.Hamdy@tera.ltd>
2026-03-10 22:40:24 +0200

    fix sales bugs

--- src/sharedComponents/stepper/StepperActions.vue
+++ src/sharedComponents/stepper/StepperActions.vue
@@ -21,17 +21,17 @@ const isLast = computed(() => props.current === props.total - 1);
 
 <template>
   <div class="flex justify-end gap-3 mt-8">
-    <button v-if="!isFirst" @click="$emit('previous')" class="min-w-30 border border-primary-500 bg-white text-primary-500
+    <button v-if="!isFirst" @click="$emit('previous')" class="min-w-25 border border-primary-500 bg-white text-primary-500
              hover:bg-gray-50 rounded-lg px-5 py-2 flex items-center gap-2 transition">
       <VsxIcon :iconName="locale === 'ar' ? 'ArrowRight' : 'ArrowLeft'" :size="18" />
       {{ prevText ||  t("items.previous")}}
     </button>
-    <button v-if="!isLast" @click="$emit('next')" class="min-w-30 bg-primary-500 hover:bg-primary-600 text-white
+    <button v-if="!isLast" @click="$emit('next')" class="min-w-25 bg-primary-500 hover:bg-primary-600 text-white
              rounded-lg px-5 py-2 flex items-center gap-2 transition">
       {{ nextText || t("items.next") }}
       <VsxIcon :iconName="locale === 'ar' ? 'ArrowLeft' : 'ArrowRight'" :size="18" />
     </button>
-    <button v-if="isLast && !isView" @click="$emit('finish')" class="min-w-30 bg-primary-600 hover:bg-primary-700 text-white
+    <button v-if="isLast && !isView" @click="$emit('finish')" class="min-w-25 bg-primary-600 hover:bg-primary-700 text-white
              rounded-lg px-5 py-2 transition">
       {{ finishText || t("button.save") }}
     </button>
