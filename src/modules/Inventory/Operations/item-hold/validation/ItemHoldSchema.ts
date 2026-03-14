b3145e10e02e72fa058766a0b75fa26d83cf4780
beshoyfayek <Beshoy.Medhat@tera.ltd>
2026-03-14 10:00:15 +0200

    new v

--- src/modules/Inventory/Operations/item-hold/validation/ItemHoldSchema.ts
+++ src/modules/Inventory/Operations/item-hold/validation/ItemHoldSchema.ts
@@ -4,7 +4,7 @@ export const ItemHoldSchema = yup.object({
   itemId: yup.string().required("form.fieldRequired"),
   warehouseId: yup.string().required("form.fieldRequired"),
   holdQuantity: yup.number().required("form.fieldRequired").min(1, "form.min_1"),
-  reason: yup.string().nullable(),
+  reason: yup.string().required("form.fieldRequired"),
   holdDate: yup.date().required("form.fieldRequired"),
   itemHoldStatus: yup.string().required("form.fieldRequired"),
   serialsForBlocking: yup.array().of(yup.string()).nullable(),
