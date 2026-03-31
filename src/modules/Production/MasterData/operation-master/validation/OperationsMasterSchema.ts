3089be0da03e653425f947670ddd09ad22131652
karemahamdy20 <K.Hamdy@tera.ltd>
2026-03-31 09:20:07 +0200

    ..

--- src/modules/Production/MasterData/operation-master/validation/OperationsMasterSchema.ts
+++ src/modules/Production/MasterData/operation-master/validation/OperationsMasterSchema.ts
@@ -16,15 +16,16 @@ export const OperationsMasterSchema = yup.object({
     )
     .typeError("form.mustBeNumber")
     .required("form.fieldRequired")
-    .moreThan(0, "form.mustBeGreaterThanZero"),
+    .moreThan(-1, "form.mustBeGreaterThanZero"),
 
   overheadPercentage: yup
     .number()
     .transform((value, originalValue) =>
       originalValue === "" ? undefined : value
     )
+    .nullable()
+    .notRequired()
     .typeError("form.mustBeNumber")
-    .required("form.fieldRequired")
     .min(0, "form.minZero")
     .max(100, "form.maxHundred"),
 });
